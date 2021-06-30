ba3eb476d3f2a56c7c1b73d63dc6809bafbb151f
StephenMoriarty <stephenwmo@gmail.com>
2021-06-30 18:13:57 -0400

    alligator sentences

---
+++ basics/2_datatypes/exercise2_1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+class exercise2_1{
+      public static void main(String[] args){
+            System.out.println(".");
+            //One way to type this out:
+            System.out.println("One way to type this out:");
+            System.out.println(".");
+            int y = 5;
+            double money = 99.99;
+            System.out.print("I will sell my ");
+            System.out.print(y);
+            System.out.print(" alligators");
+            System.out.print(" for ");
+            System.out.print(money);
+            System.out.println("$.");
+            System.out.println("That is a steal and you know it!");
+            System.out.println(".");
+            //Different way to type that out:
+            System.out.println("Different way to type this out:");
+            System.out.println(".");
+            System.out.println("I will sell my " + y + " alligators" + " for " + money + "$.");
+            System.out.println("That is a steal and you know it!");
+           
+            
+            
+            
+
+      }
+
+}
